e28361d5364cdddf748799c3ae01e019eed9f750
Lyd-W <209948061+Lyd-W@users.noreply.github.com>
2025-10-07 14:25:35 +0100

    Create function to set selectedDifficulty on click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,7 @@
 const playerSectionRef = document.querySelector("#setup-section");
 const quizContainerRef = document.querySelector(".quiz-container"); /** not yet used */
 const usernameRef = document.querySelector("#player-name"); /** not yet used */
-const difficultyRef = document.querySelector("[data-difficulty]");
-const difficultyButtonRef = document.querySelectorAll(".btn-difficulty");
+const difficultyButtonRef = Array.from(document.querySelectorAll(".btn-difficulty"));
 const easyButtonRef = document.querySelector("#easy-btn"); /** not yet used */
 const mediumButtonRef = document.querySelector("#medium-btn"); /** not yet used */
 const hardButtonRef = document.querySelector("#hard-btn"); /** not yet used */
@@ -22,21 +21,42 @@ const result = document.querySelector("#results"); /** not yet in html or js */
 const finalResultRef = document.querySelector("#final-result"); /** not yet used */
 const resultsSectionRef = document.querySelector("#results-section"); /** not yet used */
 const newGameRef = document.querySelector("#new-game-button"); /** not yet used */
+let selectedDifficulty = null;
 
 /** Hide/Show sections */
 const hideSection = (section) => section.classList.add("hide");
 const showSection = (section) => section.classList.remove("hide");
 
 /** Set question difficulty */
-difficultyButtonRef
-    .addEventListener("click", function() {
-        let selectedDifficulty = difficultyRef.dataset;
-        console.log(selectedDifficulty)
-})
+difficultyButtonRef.forEach((button) => {
+    button.addEventListener("click", (event) => {
+        event.preventDefault();
+        selectedDifficulty = event.target.dataset.difficulty;
+    });
+});
 
-/** Fetching API data */
-startButtonRef
-  .addEventListener("click", function () {
+/** Rearrange answer order to random */
+function rearrangeAnswers(results) {
+    return results.map((apiQuestion) => {
+      const answers = [
+        ...apiQuestion.incorrect_answers,
+        apiQuestion.correct_answer,
+      ];
+      answers.sort(() => Math.random() - 0.5);
+  
+      console.log(results);
+      return {
+        difficultyRef: apiQuestion.difficultyRef,
+        question: apiQuestion.question,
+        answers: answers,
+      };
+    });
+  }
+  
+  startButtonRef.addEventListener("click", getQuestions);
+  
+  /** Fetching API data and error handling for API */
+  function getQuestions() {
     fetch(
       `https://opentdb.com/api.php?amount=10&category=17&difficulty=${selectedDifficulty}&type=multiple`
     )
@@ -46,38 +66,22 @@ startButtonRef
         }
         hideSection(playerSectionRef);
         showSection(questionSectionRef);
-        return response.json();
+        return response.json(); 
       })
       .then((apiData) => {
-    const rearranged = rearrangeAnswers(apiData.results);
-    console.log("Rearranged answers:", rearranged);
-  })
-
-  .catch((error) => {
-    errorHandling(error);
-    console.error(error);
-  });
-});
-
-/** Rearrange answer order to random */
-function rearrangeAnswers(results) {
-  return results.map((apiQuestion) => {
-    const answers = [
-      ...apiQuestion.incorrect_answers,
-      apiQuestion.correct_answer,
-    ];
-    answers.sort(() => Math.random() - 0.5);
-
-    console.log(results);
-    return {
-      difficultyRef: apiQuestion.difficultyRef,
-      question: apiQuestion.question,
-      answers: answers,
-    };
-  });
-}
-
-/** Error handling for API */
-function errorHandling() {
-  alert("Questions could not be loaded at this time, please try again later.");
-}
+        const rearranged = rearrangeAnswers(apiData.results);
+        console.log("Rearranged answers:", rearranged);
+      })
+ 
+      .catch((error) => {
+        errorHandling(error);
+        console.error(error);
+      });
+  };
+  
+  function errorHandling(message) {
+    alert(
+      "Questions could not be loaded at this time, please try again later.",
+      message
+    );
+  }
